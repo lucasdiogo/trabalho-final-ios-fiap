a2525893d83d669a21e1d6b43e81d42ef8d8ad34
F5115299 Jonas Rodrigues de Oliveira <F5115299@bb.com.br>
2022-08-23 22:00:49 -0300

    model init

---
+++ LucasRafaelBrunoJonasVictor/Model/Problem.swift
@@ -0,0 +1,16 @@
+//
+//  Problem.swift
+//  LucasRafaelBrunoJonasVictor
+//
+//  Created by Jonas Rodrigues de Oliveira on 23/08/22.
+//
+
+import Foundation
+
+final class Car: Codable {
+    var _id: String?
+    var name: String = ""
+    var localization: String = ""
+    var dateRecord: String = ""
+    var description: String = ""
+}
